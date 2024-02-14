dfeb82a1c8cc9410dbe2f03788d7569695dc013f
TrippBishopStats <dyne_erg@yahoo.com>
2024-02-14 16:06:35 -0700

    added primary key and foreign key contraint to crimes table.

--- scripts/create_crime_table.sql
+++ scripts/create_crime_table.sql
@@ -73,3 +73,12 @@ SELECT
 	last_occurrence_date, reported_date, incident_address, geo_x, geo_y, geo_lon, geo_lat,
 	district_id, precinct_id, neighborhood_id, victim_count, c_year, c_month, c_day, c_dow
 FROM crime_2021;
+
+ALTER TABLE crimes
+ADD PRIMARY KEY (offense_id);
+
+-- add foreign key to the offense_codes table for more efficient and valid joins
+ALTER TABLE crimes
+ADD CONSTRAINT fk_offense_code 
+FOREIGN KEY (offense_code, offense_code_extension) 
+REFERENCES offense_codes (offense_code, offense_code_extension);
